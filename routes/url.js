d4a421c18790a3a61438b71e37804161d5a591ad
rapheal.cavalcante <raphael.cavalcante@timeitn.com.br>
2017-11-22 10:50:19 -0400

    adding some routes to user

--- routes/url.js
+++ routes/url.js
@@ -1,3 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  connection.query('select * from user', function(error, results, fields){
+    if(error){
+      res.send(JSON.stringify({"status":500, "error": error, "response":null}));
+    }else{
+      res.send(JSON.stringify({"status":200,"error":null,"response":results}));
+    }
+  });
+});
+router.post('/', function(req, res, next){
+  var input = JSON.parse(JSON.stringify(req.body));
+  var data ={
+    "id":input.id
+  };
+  connection.query('insert into user set ?',[data], function(error, results, fields){
+    if(error.code == "ER_DUP_ENTRY"){
+      res.status('409')
+      res.send("conflict");
+    }else{
+      res.send(JSON.stringify({"status":200,"error":null,"response":results}));
+    }
+  });
+});
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  connection.query('select * from users', function(error, results, fields){
+  connection.query('select * from user', function(error, results, fields){
     if(error){
       res.send(JSON.stringify({"status":500, "error": error, "response":null}));
     }else{
@@ -11,23 +11,21 @@ router.get('/', function(req, res, next) {
     }
   });
 });
+router.post('/:?/urls', function(req,res, next){
 
+});
 router.post('/', function(req, res, next){
   var input = JSON.parse(JSON.stringify(req.body));
   var data ={
-    "name":input.name,
-    "surname":input.surname,
-    "phone":input.phone,
-    "email":input.email,
-    "address":input.address,
-    "zipcode":input.zipcode
+    "id":input.id
   };
-  res.send(data);
-  /*connection.query('insert into users values ', function(error, results, fields){
-    if(error){
-      res.send(JSON.stringify({"status":500, "error": error, "response":null}));
+  connection.query('insert into user set ?',[data], function(error, results, fields){
+    if(error.code == "ER_DUP_ENTRY"){
+      res.status('409')
+      res.send("conflict");
     }else{
+      res.send(JSON.stringify({"status":200,"error":null,"response":results}));
     }
-  });*/
+  });
 });
 module.exports = router;
